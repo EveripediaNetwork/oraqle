2542965b41e2b3876ee303bd453c5d031912b34e
Christian Deciga <christian@everipedia.com>
2021-01-12 03:18:34 -0800

    Added comment

--- packages/smart-contracts/contracts/associated-press/Jan2021SenateRunoffs.sol
+++ packages/smart-contracts/contracts/associated-press/Jan2021SenateRunoffs.sol
@@ -3,6 +3,21 @@ pragma solidity ^0.6.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 
+/*
+
+
+oooooo   oooooo     oooo            .o8         .oooo.   
+ `888.    `888.     .8'            "888       .dP""Y88b  
+  `888.   .8888.   .8'    .ooooo.   888oooo.        ]8P' 
+   `888  .8'`888. .8'    d88' `88b  d88' `88b     <88b.  
+    `888.8'  `888.8'     888ooo888  888   888      `88b. 
+     `888'    `888'      888    .o  888   888 o.   .88P  
+      `8'      `8'       `Y8bod8P'  `Y8bod8P' `8bd88P'   
+
+
+*/
+
+
 /**
  * @dev Implementation of the January 5th, 2021 Senate Run-off Oracle with Associated Press
  *
