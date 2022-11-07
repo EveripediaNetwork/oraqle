4321301f125eaf5072f29dcce827fa07153a72ac
César Rodríguez <kesarr@gmail.com>
2022-11-07 19:15:24 +0100

    Update index.js

--- packages/website/pages/index.js
+++ packages/website/pages/index.js
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>BrainDAO OraQles</h1>
+        <h1 className={styles.title}>IQ OraQles</h1>
 
         {/* <p className={styles.description}>
           Get started by editing{' '}
